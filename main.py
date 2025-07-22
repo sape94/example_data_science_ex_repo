973e2dcaee9392e52c0051634b74ea9f31ffdbb5
Sergio <sergioapelayoe@gmail.com>
2025-07-22 09:31:39 -0600

    feat: add lines to main.py to perform first debug using analysis module

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from antenna-case-of-study!")
+from source.analysis import GapAnalysis
 
 
 if __name__ == "__main__":
-    main()
+    path = './data/data.csv'
+    streaming_service = 'Netflix'
+    gap_instance = GapAnalysis(path_to_data=path,
+                               streaming_service=streaming_service)
+    gap_instance.merge_tv_counts(gap_instance._tv_counts_df())
+    print(gap_instance.df.head())
--- source/analysis.py
+++ source/analysis.py
@@ -9,20 +9,32 @@ class GapAnalysis:
                  application_column: str = 'application'):
         self.path_to_data = path_to_data
         
-        df = self.load_data(path_to_data)
-        self.df = df[df[application_column].isin(streaming_service)].reset_index(drop=True)
+        df = self._load_data()
+        self.df = df[df[application_column].isin([streaming_service])].reset_index(drop=True)
         
         self.streaming_service = streaming_service
         self.application_column = application_column
         
-    def load_data(file_path: str) -> pd.DataFrame:
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"The file '{file_path}' was not found.")
+    def _load_data(self) -> pd.DataFrame:
+        if not os.path.exists(self.path_to_data):
+            raise FileNotFoundError(f"The file '{self.path_to_data}' was not found.")
         
-        if file_path.endswith('.csv'):
-            df = pd.read_csv(file_path)
+        if self.path_to_data.endswith('.csv'):
+            df = pd.read_csv(self.path_to_data)
             print('Dataframe loaded successfully.')
             return df
         else:
             raise ValueError("Unsupported file format. Please provide a CSV file.")
+        
+    def _tv_counts_df(self,
+                      tv_id_col: str = 'tv_id') -> pd.DataFrame:
+        tv_counts = self.df[tv_id_col].value_counts().reset_index()
+        tv_counts.columns = [tv_id_col, 'count']
+        return tv_counts
+    
+    def merge_tv_counts(self, 
+                        tv_counts_df: pd.DataFrame, 
+                        tv_id_col: str = 'tv_id') -> pd.DataFrame:
+        self.df = self.df.merge(tv_counts_df, on=tv_id_col, how='left')
+        self.df = self.df[self.df['count'] > 1].reset_index(drop=True)
         
